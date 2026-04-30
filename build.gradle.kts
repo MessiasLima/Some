06f2b1958d0cdd67479f6ea1096338baf8211ca0
Messias Junior <messiaslima.03@gmail.com>
2026-04-30 17:34:08 +0100

    detekt and git hook setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.gitHooks)
 }
 
 group = "dev.appoutlet"
@@ -12,10 +14,23 @@ repositories {
 dependencies {
     implementation(libs.kotlin.reflect)
     testImplementation(libs.kotlin.test)
+    detektPlugins(libs.detekt.formatting)
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+detekt {
+    autoCorrect = true
+    parallel = true
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/detekt/detekt.yml")
+    source.setFrom("src/main/kotlin", "src/test/kotlin")
+}
+
+tasks.named("prepareKotlinBuildScriptModel") {
+    dependsOn(":installGitHooks")
 }
 
 tasks.test {
